7f8037152bbaa98f3523ab958a8ac5644ae08d83
Antony CASTANER <antony.castanerpynov.com>
2019-12-05 20:36:13 +0100

    Add comments

--- js/Rules.js
+++ js/Rules.js
@@ -1,18 +1,25 @@
 class Rules {
 
     constructor() {
+        // Different rules
         this.rules = [
             "Sujet national",
             "5 défis au choix",
             "Travail d'équipe"
         ];
+        // Count for the rule creation
         this.count = 0;
+        // Select the main
         this.main = document.getElementsByTagName("main")[0];
+        // The process for the setInterval
         this.process = null;
 
         this.showTitle();
     }
 
+    /**
+     * Show the title of the rules
+     */
     showTitle() {
         let ruleTitle = document.createElement("h2");
         ruleTitle.innerHTML = "Quelles sont les règles ?";
@@ -21,6 +28,9 @@ class Rules {
         this.process = setInterval(this.showOneRule, 2000);
     }
 
+    /**
+     * Show only one rule thanks to the count
+     */
     showOneRule() {
         console.log(rules.count);
         if (rules.count < 3) {
@@ -31,14 +41,18 @@ class Rules {
             rules.count++;
         } else {
             clearInterval(rules.process);
-            setTimeout(rules.deleteRules, 2500);
+            setTimeout(rules.removeRules, 2500);
         }
     }
 
-    deleteRules() {
+    /**
+     * Remove the section rules
+     */
+    removeRules() {
         let rules = document.getElementsByClassName("rule");
         for (let i = 2; i >= 0; i--) {
             rules[i].remove();
         }
+        document.getElementsByClassName("slideTitle")[0].remove();
     }
 }
--- js/Why.js
+++ js/Why.js
@@ -1,6 +1,25 @@
 class Why {
 
     constructor() {
+        // Different results
+        this.results = [
+            "Cernes sous les yeux",
+            "Fatigue",
+            "Dépression",
+            "Tenues adéquates",
+            "A manger"
+        ];
+        // Select the main tag
+        this.main = document.getElementsByTagName("main")[0];
+        // Show the section title
+        this.showTitle();
+    }
 
+    /**
+     * Show the title section
+     */
+    showTitle() {
+        let whyTitle = document.createElement("h2");
+        this.main.appendChild(whyTitle);
     }
 }
